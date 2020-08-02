e1bff9458b83eeb99f0dae3aa55793c0305fc6b9
Naftali Weiss <naftaliweiss21@gmail.com>
2020-08-02 15:32:12 -0400

    started setting up cardApi app

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+const app = angular.module('CardAlbum', [])
+
+app.controller('AlbumController', ['$http', function($http){
+    //get the this
+    const authCtrl = this;
+    // ================================== //
+    //        Get All the Cards           //
+    // ================================== //
+    this.number = 613
+}]);// colosing the Album Controller 
