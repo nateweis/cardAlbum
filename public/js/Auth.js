b55682f6f692eb8d16bea4a51e189e0d0f4eff8c
Naftali Weiss <Naftali@PROPUMP.local>
2020-08-06 10:23:59 -0400

    input box user has to be in the db

--- public/js/Auth.js
+++ public/js/Auth.js
@@ -1,6 +1,11 @@
 export const auth = ['$http', '$window', 'cardScopeService', function($http, $window, cardScopeService){
-    const authCtrl = this
+    const authCtrl = this;
+
+    this.logInScreen = true;
+    this.changeLoginType = bool => authCtrl.logInScreen = bool
     this.currentUser = {};
+    this.username = '';
+    this.password = '';
 
     // Check if logged in
     $window.onload = () => {
@@ -15,14 +20,20 @@ export const auth = ['$http', '$window', 'cardScopeService', function($http, $wi
         $http({
             method: 'POST',
             url: '/users',
-            data: {username: 'nate', password: 'nate'}
+            data: {username: this.username, password: this.password}
         })
         .then((res)=>{ 
-            // console.log(res.data)
             authCtrl.currentUser = res.data.data
             cardScopeService.activateGetCards();
+            if(res.data.message !== "you are logged in") alert('Wrong Username or Password');
+            else{
+                authCtrl.username = '';
+                authCtrl.password = '';
+            }
          })
-        .catch((err)=>{console.log(err)})
+        .catch((err)=>{
+            if(err.data.message) alert('Wrong Username or Password');
+        })
     }
 
      // ================================== //
