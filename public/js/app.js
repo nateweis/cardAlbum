97ef80648a651690efcafc9687bcf3f6d631e072
Naftali Weiss <naftaliweiss21@gmail.com>
2020-08-08 23:33:00 -0400

    made a custome filter to filter out cards properly

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,18 @@ import {album} from './Album.js'
 
 const app = angular.module('CardAlbum', []);
 
+app.filter('cardFilter', function() {
+    const newArr = (arr, key, val) => {
+        let temp = []
+       arr.forEach(card => {
+           if(card[key] === val){temp.push(card)}
+           else if (key === 'atk' && card[key] !== null){temp.push(card)}
+       });
+       return temp 
+    }
+    return newArr
+})
+
 app.controller('AuthController', auth );
 app.controller('AlbumController', album);
 
